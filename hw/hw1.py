58d8d5d3c1a45c0613d47581f6dd8072a3444799
Саша <Sasa@MacBook-Pro-Sasha.local>
2017-09-20 15:25:04 +0300

    3

---
+++ hw/hw1.py
@@ -0,0 +1,14 @@
+a = int (input('Введите число a: '))
+b = int (input('Введите число b: '))
+c = int (input('Введите число c: '))
+if a *b == c:
+    print ('a умножить на b равно c')
+else:
+    print ('a умножить на b не равно c')
+if b == 0:
+    print ('ошибка')
+elif a / b == c:
+    print ('a разделить на b равно c')
+else:
+    print ('a разделить на b не равно c')
+    
