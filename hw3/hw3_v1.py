cb85f885599d63ef3e8a2b8ce4ce9463c6dd220c
sashmink <31881585+sashmink@users.noreply.github.com>
2017-11-12 15:40:04 +0300

    Delete hw3_v1.py

--- hw3/hw3_v1.py
+++
@@ -1,7 +0,0 @@
-stroka = input('введите строку: ')
-print(stroka)
-stroka = list(stroka)
-for num, element in enumerate(stroka):
-    stroka = stroka[:-1]
-    itog = ''.join(stroka)
-    print(itog)
