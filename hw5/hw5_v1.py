63e3840357418be44d14645fcb923f7e3d804cff
sashmink <31881585+sashmink@users.noreply.github.com>
2017-11-27 22:54:20 +0300

    Update hw5_v1.py

--- hw5/hw5_v1.py
+++ hw5/hw5_v1.py
@@ -1,19 +1,18 @@
 list = []
 s = ' '
 while s:
-    s = input('введите слово: ');
+    s = input('введите слово: ')
     list.append(s)
-with open("result.txt", "a", encoding = "utf-8") as f:
+with open("result.txt", "w", encoding = "utf-8") as f:
     for i in list:
-        d = len(i)
         res = ''
+        d = len(i)
         while d > 0:
             if d % 3 != 0:
                 res = res + str(i[d-1])
-                print(res)
             d = d - 1
-    f.write(str(res))
-    f.write("/n")
+        print(res)
+        f.write(res + '\n')
         
  
    
