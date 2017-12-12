be2d6f0bea19310238829eec92505ee4f668e485
sashmink <31881585+sashmink@users.noreply.github.com>
2017-12-12 13:25:16 +0300

    Add files via upload

---
+++ kr1/proga3.py
@@ -0,0 +1,16 @@
+list = []
+s = ' '
+while s:
+    s = input('введите название языка: ')
+    list.append(s)
+
+for name in list:
+    with open ("C:/Users/student/Desktop/kr/Extinct_languages.tsv", encoding="utf-8") as f:
+        for line in f:
+            line = line.rstrip('\n')
+            a = line.split('\t')
+            if a[0] == name:
+                print(a[0]+'-'+a[1]+'-'+a[2])
+                break
+            
+        
