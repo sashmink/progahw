77761e8379c925652342010fa088f3cd95fdc9c4
sashmink <31881585+sashmink@users.noreply.github.com>
2018-04-02 20:41:24 +0300

    Add files via upload

---
+++ hw12/p12.py
@@ -0,0 +1,16 @@
+import re
+
+a = 0
+with open("bolivia.htm", encoding="utf-8") as f:
+    lines = f.readlines()
+    for line in lines:
+        crit = re.search('Столица', line)
+        a += 1
+        if crit:
+            tsel = lines[a+1]
+            result_raw = re.search('>[А-Я][А-Яа-я-]*<', tsel)
+            result = result_raw.group()
+            result = result[1:-1]
+            break
+print('ОТВЕТ', result)
+            
