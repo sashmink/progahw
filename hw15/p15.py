10a8e72d6115e65bf8287cea32cd04955e5475e1
sashmink <31881585+sashmink@users.noreply.github.com>
2018-05-20 21:48:58 +0300

    Update p15.py

--- hw15/p15.py
+++ hw15/p15.py
@@ -5,17 +5,12 @@ tec = 0
 best = 0
 start_path = '.'
 for root, dirs, files in os.walk(start_path):
-    print (root)
-    print(len(dirs))
-    print(dirs)
     stro = str(root)
     for letter in stro:
         if letter == '/':
             tec += 1
-            print (tec)
     if tec >= best:
         best = tec
-        print ('BEST ' + str(best))
     tec = 0
 print (best)
         
